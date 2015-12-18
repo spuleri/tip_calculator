47927df277f4f887c17051c1d7bae1bd282a2413
Sergio Puleri <spuleri@ufl.edu>
2015-12-18 16:23:54 -0500

    add slideout drawer and settings page

---
+++ tip_calculator/MenuDrawerView.swift
@@ -0,0 +1,59 @@
+//
+//  MenuDrawerView.swift
+//  tip_calculator
+//
+//  Created by Sergio Puleri on 12/18/15.
+//  Copyright © 2015 Sergio Puleri. All rights reserved.
+//
+
+import UIKit
+
+class MenuDrawerView: UIView {
+
+    @IBOutlet weak var settingsButton: UIButton!
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var recentsButton: UIButton!
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    var controller : ViewController!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder);
+    }
+
+    
+    func configure(view:UIView, controller:ViewController) {
+        print("frame height is: \(view.frame.height) frame width is: \(view.frame.width)")
+        print("bounds height is: \(view.bounds.height) bounds width is: \(view.bounds.width)")
+        
+        self.frame = CGRectMake(view.frame.width, view.frame.height, widthOfElements(), frame.height)
+        self.controller = controller
+        
+    }
+    
+    func widthOfElements() -> CGFloat {
+        let width = recentsButton.frame.origin.x + recentsButton.frame.width + 50
+        print("width is: \(width)")
+        return width
+        
+    }
+    
+    @IBAction func recentTotalsClicked(sender: AnyObject) {
+        self.controller.recentTotalsButtonClicked()
+    }
+    
+    @IBAction func settingsClicked(sender: AnyObject) {
+        self.controller.settingsButtonClicked()
+    }
+    
+    @IBAction func cancelClicked(sender: AnyObject) {
+        self.controller.cancelButtonClicked()
+    }
+
+}
--- tip_calculator/ViewController.swift
+++ tip_calculator/ViewController.swift
@@ -20,12 +20,19 @@ class ViewController: UIViewController {
     // Class Properties
     // Tip Percent defaults to 15%
     var tipPercent = 15
+    var menuDrawerView : MenuDrawerView!
+    var menuDrawerToggled = false
+    var blackView : UIView!
+    var blurredEffectView : UIVisualEffectView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         tipPercentLabel.text = "15%"
         totalLabel.text = "$0.00"
+        configureBlur()
+        configureBlackView()
+        configureMenuDrawerView()
     }
 
     override func didReceiveMemoryWarning() {
@@ -33,6 +40,72 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func configureBlackView() {
+        // Create view to cover the screen when menu drawer is active
+        self.blackView = UIView(frame: CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height))
+        self.blackView.backgroundColor = UIColor.blackColor()
+        self.blackView.alpha = 0.0
+        self.view.addSubview(blackView)
+    }
+    
+    func configureMenuDrawerView() {
+        menuDrawerView = NSBundle.mainBundle().loadNibNamed("MenuDrawerView", owner: self, options: nil)[0] as? MenuDrawerView
+        let navBarView = self.navigationController?.navigationBar
+        menuDrawerView.configure(navBarView!, controller: self)
+        self.view.addSubview(menuDrawerView)
+//        let swipeDown: UISwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: "dismissAddPollView")
+//        swipeDown.direction = UISwipeGestureRecognizerDirection.Down
+//        self.menuDrawerView.addGestureRecognizer(swipeDown)
+    }
+    
+    func showMenuDrawerView() {
+        
+        UIView.animateWithDuration(0.4, delay: 0.0, options: .CurveEaseInOut, animations: {
+            var viewFrame = self.menuDrawerView.frame
+            viewFrame.origin.x = self.view.frame.size.width - viewFrame.size.width
+            self.menuDrawerView.frame = viewFrame
+            self.blackView.alpha = 0.5
+            }, completion: { finished in })
+    }
+    func hideMenuDrawerView() {
+        
+        UIView.animateWithDuration(0.4, delay: 0.0, options: .CurveEaseInOut, animations: {
+            var viewFrame = self.menuDrawerView.frame
+            viewFrame.origin.x = self.view.frame.size.width
+            self.menuDrawerView.frame = viewFrame
+             self.blackView.alpha = 0.0
+            }, completion: { finished in })
+    }
+    
+    @IBAction func menuButtonClicked(sender: AnyObject) {
+        if (!menuDrawerToggled){
+            menuDrawerToggled = true
+            showMenuDrawerView()
+            addBlur()
+        }
+        else {
+            menuDrawerToggled = false
+            hideMenuDrawerView()
+            hideblur()
+        }
+        
+    }
+    func configureBlur() {
+        let blurEffect = UIBlurEffect(style: .Light)
+        self.blurredEffectView = UIVisualEffectView(effect: blurEffect)
+        self.blurredEffectView.frame = self.view.bounds
+        self.blurredEffectView.alpha = 0.6
+        
+        let tapBlurView: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "menuButtonClicked:")
+        self.blurredEffectView.addGestureRecognizer(tapBlurView)
+    }
+    func addBlur() {
+        self.view.insertSubview(self.blurredEffectView, belowSubview: self.menuDrawerView)
+    }
+    func hideblur() {
+        self.blurredEffectView.removeFromSuperview()
+        
+    }
 
     @IBAction func onNewBillAmount(sender: AnyObject) {
         calculateTip()
@@ -56,6 +129,20 @@ class ViewController: UIViewController {
         let total = billAmount + tip
         tipAmountLabel.text = String(format: "$%.2f", arguments: [tip])
         totalLabel.text = String(format: "$%.2f", arguments: [total])
+    }
+    
+    func cancelButtonClicked() {
+        self.menuButtonClicked(self)
+        
+    }
+    
+    func settingsButtonClicked() {
+        print("settings clicked!!!")
+        performSegueWithIdentifier("showSettings", sender: self)
+        
+    }
+    func recentTotalsButtonClicked() {
+        
     }
 }
 
